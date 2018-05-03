bff3c8c845a524ff68c9afb3a81292debf5fd6b4
Zi Zhou <zi.zhou@bigswitch.com>
2018-05-03 12:58:28 -0700

    support 25G-LR
    Conflicts:
    	packages/base/any/onlp/src/sff/module/python/onlp/sff/enums.py

--- packages/base/any/onlp/src/sff/module/python/onlp/sff/enums.py
+++ packages/base/any/onlp/src/sff/module/python/onlp/sff/enums.py
@@ -43,22 +43,24 @@ class SFF_MODULE_TYPE(Enumeration):
     _40G_BASE_SM4 = 13
     _40G_BASE_ER4 = 14
     _25G_BASE_CR = 15
-    _10G_BASE_SR = 16
-    _10G_BASE_LR = 17
-    _10G_BASE_LRM = 18
-    _10G_BASE_ER = 19
-    _10G_BASE_CR = 20
-    _10G_BASE_SX = 21
-    _10G_BASE_LX = 22
-    _10G_BASE_ZR = 23
-    _10G_BASE_SRL = 24
-    _1G_BASE_SX = 25
-    _1G_BASE_LX = 26
-    _1G_BASE_CX = 27
-    _1G_BASE_T = 28
-    _100_BASE_LX = 29
-    _100_BASE_FX = 30
-    _4X_MUX = 31
+    _25G_BASE_SR = 16
+    _25G_BASE_LR = 17
+    _10G_BASE_SR = 18
+    _10G_BASE_LR = 19
+    _10G_BASE_LRM = 20
+    _10G_BASE_ER = 21
+    _10G_BASE_CR = 22
+    _10G_BASE_SX = 23
+    _10G_BASE_LX = 24
+    _10G_BASE_ZR = 25
+    _10G_BASE_SRL = 26
+    _1G_BASE_SX = 27
+    _1G_BASE_LX = 28
+    _1G_BASE_CX = 29
+    _1G_BASE_T = 30
+    _100_BASE_LX = 31
+    _100_BASE_FX = 32
+    _4X_MUX = 33
 
 
 class SFF_SFP_TYPE(Enumeration):
@@ -66,5 +68,6 @@ class SFF_SFP_TYPE(Enumeration):
     QSFP = 1
     QSFP_PLUS = 2
     QSFP28 = 3
+    SFP28 = 4
 
 # <auto.end.pyenum(ALL).define>
